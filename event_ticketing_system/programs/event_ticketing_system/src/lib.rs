fc3c3f804c986ab2b49812a9e5e04b1b4bfabbfd
Ishita Agarwal <ishitagrawal0207@gmail.com>
2025-11-17 17:50:36 +0530

    - update the contract

--- event_ticketing_system/programs/event_ticketing_system/src/lib.rs
+++ event_ticketing_system/programs/event_ticketing_system/src/lib.rs
@@ -18,61 +18,101 @@ pub mod event_ticketing_system {
         Ok(())
     }
 
-    pub fn buy_ticket(ctx: Context<BuyTicket>, seat_no: u32) -> Result<()> {
+    pub fn buy_ticket(ctx: Context<BuyTicket>, title: String, seat_no: u32) -> Result<()> {
         let ticket = &mut ctx.accounts.ticket;
         let event = &mut ctx.accounts.event;
 
-        ticket.owner = ticket.owner.key();
+        ticket.owner = ctx.accounts.buyer.key();
         ticket.event = event.key();
         ticket.seat_no = seat_no;
         ticket.is_used = false;
         ticket.bump = ctx.bumps.ticket;
 
-        event.total_tickets += 1;
-        event.sold_count += 1;
+        event.total_tickets = event
+            .total_tickets
+            .checked_add(1)
+            .ok_or(ProgramError::Custom(0))?;
+        event.sold_count = event
+            .sold_count
+            .checked_add(1)
+            .ok_or(ProgramError::Custom(0))?;
+
         Ok(())
     }
 
-    pub fn transfer_ticket(ctx: Context<TransferTicket>, new_owner: Pubkey) -> Result<()> {
+    pub fn transfer_ticket(
+        ctx: Context<TransferTicket>,
+        _title: String,
+    ) -> Result<()> {
         let ticket = &mut ctx.accounts.ticket;
 
-        ticket.owner = new_owner;
+        require_keys_eq!(ticket.owner, ctx.accounts.current_owner.key(), EventErrors::NotOwner);
+
+        ticket.owner = ctx.accounts.new_owner.key();
         Ok(())
     }
 
-    pub fn verify_ticket(ctx: Context<VerifyTicket>) -> Result<()> {
+    pub fn verify_ticket(
+        ctx: Context<VerifyTicket>,
+        _title: String, 
+        _seat_no: u32,
+    ) -> Result<()> {
         let ticket = &mut ctx.accounts.ticket;
+        let event = &ctx.accounts.event;
+
+        require!(!ticket.is_used, EventErrors::TicketAlreadyUsed);
+        require_keys_eq!(ticket.event, event.key(), EventErrors::InvalidEvent);
+
         ticket.is_used = true;
 
         Ok(())
     }
 
-    pub fn cancel_event(ctx: Context<CancelEvent>) -> Result<()> {
+    pub fn cancel_event(ctx: Context<CancelEvent>, title: String) -> Result<()> {
+        msg!("Event {} closed", title);
         Ok(())
     }
 }
 
+#[error_code]
+pub enum EventErrors {
+    #[msg("You are not the owner of the ticket.")]
+    NotOwner,
+    #[msg("Ticket does not exist.")]
+    TicketDoesNotExist,
+    #[msg("Ticket is already used.")]
+    TicketAlreadyUsed,
+    #[msg("The event is invalid")]
+    InvalidEvent,
+}
+
 #[account]
-#[derive(InitSpace)]
 pub struct Event {
-    #[max_len(50)]
     pub title: String,
     pub date: i64,
     pub total_tickets: u64,
     pub sold_count: u64,
     pub creator: Pubkey,
-    pub bump: u8
+    pub bump: u8,
+}
+
+impl Event {
+    pub const INIT_SPACE: usize = 4 + 50 + 8 + 8 + 8 + 32 + 1;
 }
 
 #[account]
-#[derive(InitSpace)]
 pub struct Ticket {
     pub owner: Pubkey,
     pub event: Pubkey,
     pub seat_no: u32,
     pub is_used: bool,
-    pub bump: u8
+    pub bump: u8,
 }
+
+impl Ticket {
+    pub const INIT_SPACE: usize = 32 + 32 + 4 + 1 + 1;
+}
+
 
 #[derive(Accounts)]
 #[instruction(title: String)]
@@ -88,61 +128,74 @@ pub struct CreateEvent<'info> {
 
     #[account(mut)]
     pub creator: Signer<'info>,
-    pub system_program: Program<'info, System>
+    pub system_program: Program<'info, System>,
 }
 
-// Ticket → (owner, event, seat_no, QR_hash, bump) 
 
 #[derive(Accounts)]
-#[instruction(seat_no: i64)]
+#[instruction(title: String, seat_no: u32)]
 pub struct BuyTicket<'info> {
     #[account(
+        mut,
+        seeds = [b"event", title.as_bytes(), event.creator.as_ref()],
+        bump = event.bump,
+    )]
+    pub event: Account<'info, Event>,
+
+    #[account(
         init,
-        payer = owner,
+        payer = buyer,
         space = 8 + Ticket::INIT_SPACE,
-        seeds = [b"ticket", owner.key().as_ref(), event.key().as_ref(), &seat_no.to_le_bytes()],
+        seeds = [b"ticket", event.key().as_ref(), &seat_no.to_le_bytes()],
         bump
     )]
-
     pub ticket: Account<'info, Ticket>,
-    pub event: Account<'info, Event>,
 
     #[account(mut)]
-    pub owner: Signer<'info>,
-    pub system_program: Program<'info, System>
+    pub buyer: Signer<'info>,
+    pub system_program: Program<'info, System>,
 }
 
-// transfer_ticket
-
 #[derive(Accounts)]
+#[instruction(title: String, seat_no: u32)]
 pub struct TransferTicket<'info> {
     #[account(
-        mut, 
-        seeds = [b"ticket", owner.key().as_ref(), event.key().as_ref(), &ticket.seat_no.to_le_bytes()],
+        mut,
+        seeds = [b"ticket", event.key().as_ref(), &seat_no.to_le_bytes()],
         bump = ticket.bump
     )]
-
     pub ticket: Account<'info, Ticket>,
+
+    #[account(
+        seeds = [b"event", title.as_bytes(), event.creator.as_ref()],
+        bump = event.bump
+    )]
     pub event: Account<'info, Event>,
 
-    #[account(mut)]
-    pub owner: Signer<'info>,
-    pub new_owner: Signer<'info>
+    pub current_owner: Signer<'info>,
+
+    pub new_owner: Signer<'info>,
 }
 
+//
+//
 #[derive(Accounts)]
+#[instruction(title: String, seat_no: u32)]
 pub struct VerifyTicket<'info> {
     #[account(
-        mut, 
-        seeds = [b"ticket", owner.key().as_ref(), event.key().as_ref(), &ticket.seat_no.to_le_bytes()],
+        mut,
+        seeds = [b"ticket", event.key().as_ref(), &seat_no.to_le_bytes()],
         bump = ticket.bump
     )]
-
     pub ticket: Account<'info, Ticket>,
+
+    #[account(
+        seeds = [b"event", title.as_bytes(), event.creator.as_ref()],
+        bump = event.bump
+    )]
     pub event: Account<'info, Event>,
 
-    #[account(mut)]
-    pub owner: Signer<'info>,
+    pub event_creator: Signer<'info>,
 }
 
 #[derive(Accounts)]
@@ -155,5 +208,6 @@ pub struct CancelEvent<'info> {
         close = creator
     )]
     pub event: Account<'info, Event>,
-    pub creator: Signer<'info>
+
+    pub creator: Signer<'info>,
 }
